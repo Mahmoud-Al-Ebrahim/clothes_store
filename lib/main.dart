9788377731a08c8f5747b7f805ef4e769c828f08
Mahmoud AlEbrahim <ma453935@gmail.com>
2025-05-13 15:31:48 +0300

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:clothes_store/constant/app_color.dart';
+import 'package:flutter/services.dart';
+import 'package:clothes_store/views/screens/welcome_page.dart';
+
+void main() {
+  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: AppColor.primary,
+    statusBarIconBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Nunito',
+      ),
+      home: WelcomePage(),
+    );
+  }
+}

8fc10f0860f8f52f6f6d59647f2e6ec7798b3e3e
batoul43 <batoulkebbeh43@gmail.com>
2025-08-28 02:09:10 +0300

    clothes store updated
    design of frofile page

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_sdk\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS TUF\\Desktop\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_sdk\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS TUF\\Desktop\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a"
+  "FLUTTER_ROOT=C:\\flutter_sdk\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS TUF\\Desktop\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS TUF\\Desktop\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a"
+  "FLUTTER_TARGET=C:\\Users\\ASUS TUF\\Desktop\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS TUF\\Desktop\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\clothes_store-6f0d2108ca0a10c6e66303c4e2e5672e00f2ec1a\\.dart_tool\\package_config.json"
+)
